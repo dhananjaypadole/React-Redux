337c88be3e787ecbae1e5ebf470e31c7f5596ca3
Dhananjay Padole <padole.dhananjay19@gmail.com>
2022-08-20 12:57:49 +0530

    video 6 done

---
+++ react-redux/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM, { render }  from 'react-dom/client'
+import HomeContainer from './Containers/HomeContainer';
+import './App.css'
+
+const App = ()=>{
+return(
+    <>
+   <HomeContainer></HomeContainer>
+    </>
+)    
+}
+
+export default App;
---
+++ react-redux/src/Containers/HomeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../Components/Home'
+
+const HomeContainer = () => {
+  return (
+    <div>Via Containers
+      <Home></Home>
+    </div>
+  )
+}
+
+export default HomeContainer
